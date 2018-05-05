276a22e86511a51b91e4d82bd4e28a3ff77051c4
Andrii Tsuran <avtsuran@gmail.com>
2018-05-05 12:15:00 +0300

    Created modal window for deleting board.

--- src/js/main.js
+++ src/js/main.js
@@ -1,11 +1,16 @@
-function show(show, hide){
+function changeDisplayForTwoElements(show, hide){
     document.getElementById(show).style.display = "block";
     document.getElementById(hide).style.display = "none";
 }
 
-function invokeDeleteButton(id) {
+function showOrHideElement(id) {
     if(document.getElementById(id).style.display == "block")
         document.getElementById(id).style.display = "none";
     else
         document.getElementById(id).style.display = "block";
+}
+
+function deleteBoard(id) {
+    document.getElementById("modal").style.display = "block";
+    document.getElementById("delete-board-by-id").href = "/delete-board?id=" + id;
 }
